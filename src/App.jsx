b56b32d408bf1879b76928127a541471bdd47d9e
Emilse Arias <emilsearias@gmail.com>
2024-02-14 17:45:19 -0300

    realizamos una consulta a la api de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+  
+  useEffect(()=>{
+    consultarAPI();
+  },[])
+
+  const consultarAPI = async ()=>{
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const dato = await respuesta.json()
+    console.log(respuesta);
+    console.log(dato[0]);
+  }
 
   return (
     <>
